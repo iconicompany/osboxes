c22e478bf3bcd1b4e97df682d18ddc8aa0b98eeb
Vjacheslav Borisov <slavb@ilb.ru>
2020-08-06 15:00:55 +0400

    php config

---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php
+
+  setlocale(LC_ALL, "POSIX");
+  setlocale(LC_TIME, "ru_RU.UTF-8");
+  setlocale(LC_CTYPE, "ru_RU.UTF-8");
+  setlocale(LC_COLLATE, "ru_RU.UTF-8");
+  putenv("LANG=ru_RU.UTF-8");
+  putenv("LC_ALL=ru_RU.UTF-8");
+  //var_dump(setlocale(LC_ALL, 0));
+  umask(027);
+
+  $_SERVER["TMPDIR"] = sys_get_temp_dir();
+  $_SERVER["ILB_SSLCACERTIFICATEFILE"] = "/etc/ssl/ourCAbundle.crt";
