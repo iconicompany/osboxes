58fc24ee2ab71e7f626ba45596337baf4713d8d4
Vjacheslav Borisov <slavb@ilb.ru>
2022-05-20 11:14:22 +0400

    add comment viber

--- ubuntu/apps/viber.sh
+++ ubuntu/apps/viber.sh
@@ -2,6 +2,7 @@ cd /tmp
 wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
 sudo apt install ./viber.deb
 rm -f viber.deb
+# https://askubuntu.com/questions/1403319/viber-no-connection-on-ubuntu-22-04-even-i-have-an-internet
 wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.3_amd64.deb
 sudo dpkg -i libssl1.1_1.1.1l-1ubuntu1.3_amd64.deb
 rm -f libssl1.1_1.1.1l-1ubuntu1.3_amd64.deb
