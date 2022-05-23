91e453115e4bd5e25500a843ca4ab830639a91a4
Vjacheslav Borisov <slavb@ilb.ru>
2022-05-23 11:59:33 +0400

    golang,zoom

---
+++ ubuntu/apps/golang.sh
@@ -0,0 +1,2 @@
+# https://linuxconfig.org/how-to-install-go-on-ubuntu-22-04-jammy-jellyfish-linux
+sudo apt install golang
---
+++ ubuntu/apps/zoom.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+rm -f zoom_amd64.deb
