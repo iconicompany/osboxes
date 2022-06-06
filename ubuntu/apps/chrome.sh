72b7d2cb908744ef54efa75c940f17bef02d48b8
Vjacheslav Borisov <slavb@ilb.ru>
2022-06-06 14:03:33 +0400

    chrome

---
+++ ubuntu/apps/chrome.sh
@@ -0,0 +1,5 @@
+set -e
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
