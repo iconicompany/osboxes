1e3c0ff60c2b7442a6efd4d4288f6e19e39c705e
Vjacheslav Borisov <slavb@ilb.ru>
2022-07-07 11:04:01 +0400

    fix nb

--- ubuntu/apps/netbeansjeddict.sh
+++ ubuntu/apps/netbeansjeddict.sh
@@ -3,4 +3,4 @@ cd /tmp
 wget -c https://github.com/jeddict/jeddict/releases/download/5.4.3/Jeddict_5.4.3_with_NetBeans_IDE_12.1.zip
 cd /opt
 sudo unzip /tmp/Jeddict_5.4.3_with_NetBeans_IDE_12.1.zip
-sudo chmod +x /opt/NetBeans_IDE_12.1/bin
+sudo chmod +x /opt/NetBeans_IDE_12.1/bin/netbeans
