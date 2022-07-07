9b3b57f3d1dd01fd283e1f5fd9c8156ad8e136dd
Vjacheslav Borisov <slavb@ilb.ru>
2022-07-07 11:02:25 +0400

    nb jeddict

---
+++ ubuntu/apps/netbeansjeddict.sh
@@ -0,0 +1,6 @@
+set -e
+cd /tmp
+wget -c https://github.com/jeddict/jeddict/releases/download/5.4.3/Jeddict_5.4.3_with_NetBeans_IDE_12.1.zip
+cd /opt
+sudo unzip /tmp/Jeddict_5.4.3_with_NetBeans_IDE_12.1.zip
+sudo chmod +x /opt/NetBeans_IDE_12.1/bin
