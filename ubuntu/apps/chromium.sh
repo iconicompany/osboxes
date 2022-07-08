2fe186f11727d7c833f81bed0667cce9d9d0a5f4
Vjacheslav Borisov <slavb@ilb.ru>
2022-07-08 12:30:54 +0400

    ppa:savoury1/ffmpeg4 breaks libopencv-dev

--- ubuntu/apps/chromium.sh
+++ ubuntu/apps/chromium.sh
@@ -1,5 +1,6 @@
 # https://www.linuxcapable.com/how-to-install-chromium-browser-on-ubuntu-22-04-lts/
 sudo add-apt-repository ppa:savoury1/chromium -y
-sudo add-apt-repository ppa:savoury1/ffmpeg4 -y
+#breaks opencv-dev, to remove issue command sudo ppa-purge ppa:savoury1/ffmpeg4^
+#sudo add-apt-repository ppa:savoury1/ffmpeg4 -y
 sudo apt-get update
 sudo apt install chromium-browser -y
