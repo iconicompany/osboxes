b1226b76932e16930211afa24597ce5e616f702b
Vjacheslav Borisov <slavb@ilb.ru>
2022-07-08 12:33:07 +0400

    separate java config

---
+++ ubuntu/apps/java.sh
@@ -0,0 +1,4 @@
+set -e
+sudo apt install openjdk-11-jdk 
+# for ru.ilb.common.jaxrs.exceptionhandler.SendMailExceptionHandler
+sudo apt-get install sendmail
--- ubuntu/apps/netbeans.sh
+++ ubuntu/apps/netbeans.sh
@@ -1,3 +1,2 @@
 set -e
-sudo apt install openjdk-11-jdk
 sudo snap install netbeans --classic
