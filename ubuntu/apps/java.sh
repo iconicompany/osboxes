01cd04570b2b23efba55b6b984217dd8abe24914
Vjacheslav Borisov <slavb@ilb.ru>
2022-09-08 14:05:07 +0400

    doc and source example

--- ubuntu/apps/java.sh
+++ ubuntu/apps/java.sh
@@ -1,4 +1,4 @@
 set -e
-sudo apt install openjdk-11-jdk 
+sudo apt install openjdk-11-jdk #openjdk-11-doc openjdk-11-source
 # for ru.ilb.common.jaxrs.exceptionhandler.SendMailExceptionHandler
 sudo apt-get install sendmail
