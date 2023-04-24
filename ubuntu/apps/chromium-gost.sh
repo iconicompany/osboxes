6755a0ac1c0f934f03969cfdd2a4a8daf903b2c1
Vjacheslav Borisov <slavb@iconicompany.com>
2023-04-24 10:29:38 +0400

    docker

--- ubuntu/apps/chromium-gost.sh
+++ ubuntu/apps/chromium-gost.sh
@@ -1,6 +1,8 @@
 #https://dev.rutoken.ru/pages/viewpage.action?pageId=72451917
 set -e
 cd /tmp
-wget https://github.com/deemru/Chromium-Gost/releases/download/102.0.5005.61/chromium-gost-102.0.5005.61-linux-amd64.deb
-sudo apt install ./chromium-gost-102.0.5005.61-linux-amd64.deb
-rm -f chromium-gost-102.0.5005.61-linux-amd64.deb
+wget -O chromuim-gost.deb https://github.com/deemru/Chromium-Gost/releases/download/111.0.5563.64/chromium-gost-111.0.5563.64-linux-amd64.deb
+#102.0.5005.61/chromium-gost-102.0.5005.61-linux-amd64.deb
+sudo apt install ./chromuim-gost.deb
+rm -f ./chromuim-gost.deb
+
