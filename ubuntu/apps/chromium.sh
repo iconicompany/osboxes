bd864564a7adaf254902d3ce4be70a9965ba7d37
Viacheslav Borisov <slavb18@iconicompany.com>
2024-08-30 06:28:43 +0400

    update

--- ubuntu/apps/chromium.sh
+++ ubuntu/apps/chromium.sh
@@ -1,6 +1,11 @@
 # https://www.linuxcapable.com/how-to-install-chromium-browser-on-ubuntu-22-04-lts/
-sudo add-apt-repository ppa:savoury1/chromium -y
+#sudo add-apt-repository ppa:savoury1/chromium -y
 #breaks opencv-dev, to remove issue command sudo ppa-purge ppa:savoury1/ffmpeg4^
 #sudo add-apt-repository ppa:savoury1/ffmpeg4 -y
-sudo apt-get update
-sudo apt install chromium-browser -y
+#sudo apt-get update
+#sudo apt install chromium-browser -y
+
+sudo add-apt-repository ppa:xtradeb/apps -y
+sudo apt update
+sudo apt install chromium
+echo "Package: chromium\nPin: release o=LP-PPA-xtradeb-apps\nPin-Priority: 700" | sudo tee /etc/apt/preferences.d/chromium-pin
---
+++ ubuntu/apps/xnview.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://download.xnview.com/XnViewMP-linux-x64.deb
+sudo apt install ./XnViewMP-linux-x64.deb
+rm -f ./XnViewMP-linux-x64.deb
