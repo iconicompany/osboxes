57d39f463c4dfe67cf5fec01b76c7fdf859ff8a6
Viacheslav Borisov <slavb18@iconicompany.com>
2024-10-11 14:52:25 +0400

    update

--- ubuntu/apps/step-cli.sh
+++ ubuntu/apps/step-cli.sh
@@ -1,6 +1,6 @@
+set -e
 if ! command -v dpkg > /dev/null; then
     LATEST_STEP_VERSION=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    #echo $LATEST_STEP_VERSION
     wget -qO - https://dl.smallstep.com/gh-release/cli/gh-release-header/${LATEST_STEP_VERSION}/step_linux_${LATEST_STEP_VERSION:1}_amd64.tar.gz | tar zxvf -  -C /tmp/
     sudo mv /tmp/step_${LATEST_STEP_VERSION:1}/bin/step /usr/local/bin
 else
