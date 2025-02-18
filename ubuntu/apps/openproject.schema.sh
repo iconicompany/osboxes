13b2689f25519d60e1db54b383375054a1e31dd1
Viacheslav Borisov <slavb18@iconicompany.com>
2025-02-18 10:26:10 +0400

    update

---
+++ ubuntu/apps/openproject.schema.sh
@@ -0,0 +1,14 @@
+echo -n "you are running $0 type yes to continue: "
+read -r answer
+[ "$answer" != "yes" ] && exit
+
+PWD=`uuidgen`
+sudo -u postgres psql << EOT
+DROP DATABASE IF EXISTS icompany;
+create database openproject;
+CREATE USER openproject WITH PASSWORD '$PWD';
+GRANT ALL PRIVILEGES ON DATABASE openproject to openproject;
+ALTER USER openproject CREATEDB;
+EOT
+
+echo "Password for user 'openproject': '$PWD'"
