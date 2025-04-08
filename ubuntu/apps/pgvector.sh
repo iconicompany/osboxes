f3637ff206b5c17b4e2d3bfc391ffaf7f0f438ee
Viacheslav Borisov <slavb18@iconicompany.com>
2025-04-08 14:42:33 +0400

    update

--- ubuntu/apps/pgvector.sh
+++ ubuntu/apps/pgvector.sh
@@ -1,3 +1,3 @@
 sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
-sudo apt install -y postgresql-14-pgvector
+sudo apt install -y postgresql-16-pgvector
 echo "CREATE EXTENSION vector;" | sudo -u postgres psql
