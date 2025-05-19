cea9d21758f5e46bac7252c43b3c485784c3aa7a
Viacheslav Borisov <slavb18@iconicompany.com>
2025-05-19 16:15:33 +0400

    update

--- ubuntu/apps/github-runner.sh
+++ ubuntu/apps/github-runner.sh
@@ -4,4 +4,4 @@ sudo -u runner bash -c "cd /home/runner;
 curl -o actions-runner-linux-x64-2.324.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.324.0/actions-runner-linux-x64-2.324.0.tar.gz;
 tar xzf ./actions-runner-linux-x64-2.324.0.tar.gz;
 rm actions-runner-linux-x64-2.324.0.tar.gz"
-
+sudo gpasswd -a runner docker
