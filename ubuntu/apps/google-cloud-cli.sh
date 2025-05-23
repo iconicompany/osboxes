2cecf5aac99e5392bf886b70bb9f4962480f4f47
Viacheslav Borisov <slavb18@iconicompany.com>
2025-05-23 11:16:08 +0400

    update

--- ubuntu/apps/google-cloud-cli.sh
+++ ubuntu/apps/google-cloud-cli.sh
@@ -1,5 +1,6 @@
 set -e
 cd /tmp
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-linux-x86_64.tar.gz
+cd /opt
+sudo tar -xf /tmp/google-cloud-cli-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
