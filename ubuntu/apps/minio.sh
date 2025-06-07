e79b78f5d5b1d32b21051e92ad0b09a704ad89b1
Viacheslav Borisov <slavb18@iconicompany.com>
2025-06-07 08:15:41 +0400

    update

--- ubuntu/apps/minio.sh
+++ ubuntu/apps/minio.sh
@@ -3,3 +3,6 @@ cd /tmp
 wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio_20250524170830.0.0_amd64.deb -O minio.deb
 sudo dpkg -i minio.deb
 rm minio.deb
+curl -o mc-minio https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc-minio
+sudo mv mc-minio /usr/local/bin
