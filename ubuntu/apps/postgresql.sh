c45b43b3927bda3d9ce4204c7f6e31f03a3e25f8
Viacheslav Borisov <slavb18@iconicompany.com>
2025-06-09 11:52:00 +0400

    update

--- ubuntu/apps/postgresql.sh
+++ ubuntu/apps/postgresql.sh
@@ -1,4 +1,4 @@
 set -e
 sudo apt install -y postgresql-common
-sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y
 sudo apt install -y postgresql postgresql-17-pgvector
