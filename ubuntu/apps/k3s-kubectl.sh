12282a7253da29088d4e7eb21e946722feb12d13
Viacheslav Borisov <slavb18@iconicompany.com>
2025-07-01 16:20:14 +0400

    update

--- ubuntu/apps/k3s-kubectl.sh
+++ ubuntu/apps/k3s-kubectl.sh
@@ -2,7 +2,9 @@ set -e
 LATEST_K3S_VERSION=$(curl -s https://api.github.com/repos/k3s-io/k3s/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 TMPDIR=`mktemp -d`
 cd  ${TMPDIR}
-curl -Lo k3s https://github.com/k3s-io/k3s/releases/download/${LATEST_K3S_VERSION}/k3s
+URL=https://github.com/k3s-io/k3s/releases/download/${LATEST_K3S_VERSION}/k3s
+echo Downloading $URL
+curl -Lo k3s $URL
 chmod a+x k3s
 sudo mkdir -p /usr/local/bin
 sudo mv -f k3s /usr/local/bin/k3s
