290c7273169d3914d1fcc56aca8641b57577e39e
Viacheslav Borisov <slavb18@iconicompany.com>
2025-12-26 19:28:21 +0400

    143

--- ubuntu/apps/chromium-gost.sh
+++ ubuntu/apps/chromium-gost.sh
@@ -1,7 +1,7 @@
 #https://dev.rutoken.ru/pages/viewpage.action?pageId=72451917
 set -e
 cd /tmp
-VERSION=134.0.6998.178
+VERSION=143.0.7499.169
 #wget -O chromuim-gost.deb https://github.com/deemru/Chromium-Gost/releases/download/111.0.5563.64/chromium-gost-111.0.5563.64-linux-amd64.deb
 wget -O chromuim-gost.deb https://github.com/deemru/Chromium-Gost/releases/download/${VERSION}/chromium-gost-${VERSION}-linux-amd64.deb
 sudo apt install ./chromuim-gost.deb
