bce5bd2b3345b88f79b944ec56b4dcb5f9504119
Ваше Имя <slavb18@iconicompany.com>
2026-08-26 18:02:40 +0000

    update

--- ubuntu/apps/werf.sh
+++ ubuntu/apps/werf.sh
@@ -1,6 +1,7 @@
 set -e -v
+VERSION=2.73.0
 cd /tmp
-curl -sSLO "https://tuf.werf.io/targets/releases/1.2.334/linux-amd64/bin/werf"
+curl -sSLO "https://tuf.werf.io/targets/releases/$VERSION/linux-amd64/bin/werf"
 chmod +x werf
 sudo mv werf /usr/local/bin
 werf
